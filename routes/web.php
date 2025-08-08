df0bb3f9ba6692eba7d4539991780149a7990c49
aliqassim21 <aliqassim170@gmail.com>
2025-08-08 12:28:15 +0300

    initial routes base on roles

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+require_once 'Admin.php';
+require_once 'School Manager.php';
+require_once 'Structure Manager.php';
+require_once 'Student.php';
+require_once 'Teacher.php';
+require_once 'User Administrator.php';

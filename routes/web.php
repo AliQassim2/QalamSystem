9dbe892545aeda2691a84009b4363cb0398a901e
aliqassim21 <aliqassim170@gmail.com>
2025-08-24 09:32:01 +0300

    add some middleware

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,21 @@ Route::middleware(\App\Http\Middleware\CustomAuth::class)->group(function () {
     Route::middleware(\App\Http\Middleware\isAdmin::class)->group(function () {
         require_once 'Admin.php';
     });
-
-    require_once 'School Manager.php';
-
+    Route::middleware(\App\Http\Middleware\isSchoolManager::class)->group(function () {
+        require_once 'School Manager.php';
+    });
     Route::middleware(\App\Http\Middleware\isStructureManager::class)->group(function () {
         require_once 'Structure Manager.php';
     });
     Route::middleware(\App\Http\Middleware\isUserAdministrator::class)->group(function () {
         require_once 'User Administrator.php';
     });
-    require_once 'Student.php';
-    require_once 'Teacher.php';
+    Route::middleware(\App\Http\Middleware\isStudent::class)->group(function () {
+        require_once 'Student.php';
+    });
+    Route::middleware(\App\Http\Middleware\isTeacher::class)->group(function () {
+        require_once 'Teacher.php';
+    });
     Route::get('{stage}', [\App\Http\Controllers\GeneralController::class, 'getStageData'])->name('account.teachers.links.data');
     Route::get('/teacher/stage/{stage}/subjects', [\App\Http\Controllers\TeacherController::class, 'getSubjects']);
     Route::get('/teacher/subject/{subject}/classes', [\App\Http\Controllers\TeacherController::class, 'getClasses']);
